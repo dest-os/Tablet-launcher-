22a7b931d83ef75407847cc841ac409a45902e2f
dest-os <destosares@gmail.com>
2026-09-13 13:14:40 +0300

    Add LiveClock widget to display current time and date

---
+++ lib/widgets/live_clock.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class LiveClock extends StatefulWidget {
+  const LiveClock({super.key});
+
+  @override
+  State<LiveClock> createState() => _LiveClockState();
+}
+
+class _LiveClockState extends State<LiveClock> {
+  late DateTime _now;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _now = DateTime.now();
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (_) {
+        if (mounted) {
+          setState(() {
+            _now = DateTime.now();
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  String _twoDigits(int value) {
+    return value.toString().padLeft(2, '0');
+  }
+
+  String _formatTime() {
+    final hour = _twoDigits(_now.hour);
+    final minute = _twoDigits(_now.minute);
+
+    return '$hour:$minute';
+  }
+
+  String _formatDate() {
+    final day = _twoDigits(_now.day);
+    final month = _twoDigits(_now.month);
+    final year = _now.year;
+
+    return '$day.$month.$year';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          _formatTime(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 42,
+            fontWeight: FontWeight.w300,
+            letterSpacing: 2,
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          _formatDate(),
+          style: const TextStyle(
+            color: Colors.white70,
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 1,
+          ),
+        ),
+      ],
+    );
+  }
+}
