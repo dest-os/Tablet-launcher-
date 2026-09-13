52c9fa9e264fbd82de5848240232b2fb24389b15
dest-os <destosares@gmail.com>
2026-09-13 15:14:14 +0300

    Create InstalledAppsService for app management
    Implement InstalledAppsService to manage installed applications.

---
+++ lib/services/installed_apps_service.dart
@@ -0,0 +1,26 @@
+import 'package:installed_apps/installed_apps.dart';
+
+class InstalledAppsService {
+  const InstalledAppsService();
+
+  Future<List<AppInfo>> getLaunchableApps() async {
+    final apps = await InstalledApps.getInstalledApps(
+      excludeSystemApps: true,
+      excludeNonLaunchableApps: true,
+      withIcon: true,
+    );
+
+    apps.sort(
+      (a, b) => a.name.toLowerCase().compareTo(
+            b.name.toLowerCase(),
+          ),
+    );
+
+    return apps;
+  }
+
+  Future<bool> launchApp(String packageName) async {
+    final result = await InstalledApps.startApp(packageName);
+    return result ?? false;
+  }
+}
